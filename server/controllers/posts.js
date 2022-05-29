10e6d39bf0c9febd0943be7f4bc98e4820287424
toey02354 <nattapoln02354@gmail.com>
2022-05-29 19:36:58 +0700

    backend boilerplate done

---
+++ server/controllers/posts.js
@@ -0,0 +1,21 @@
+import PostMessage from '../models/postsMessage.js'
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find()
+    res.status(200).json(postMessages)
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  }
+}
+
+export const createPosts = async (req, res) => {
+  const post = req.body
+  const newPost = new PostMessage(post)
+  try {
+    await newPost.save()
+    res.status(201).json(newPost)
+  } catch (err) {
+    res.status(409).json({ message: err.message })
+  }
+}
